aa792e9e73fb106c3e74b39a4570dff1be8e9aaf
Your Name <you@example.com>
2024-09-28 11:10:56 +0530

    deleted statement from ems application

--- EMS_Backend/src/main/java/com/excelr/ems_backend/EmsBackendApplication.java
+++ EMS_Backend/src/main/java/com/excelr/ems_backend/EmsBackendApplication.java
@@ -9,7 +9,6 @@ public class EmsBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
 		System.out.println("Hello World!");
-		System.out.println("Git hub is connected ");
 		
 
 		
